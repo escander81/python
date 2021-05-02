98358bfa8677cbf1d83444e65d8af928e09d438e
alex_tarassov <tarassov.sasha@gmail.com>
2021-05-02 22:51:24 +0400

    test update

--- Task_1.py
+++ Task_1.py
@@ -1,1 +1,12 @@
 print('Hello')
+# d = input(int('введите значение переменной а: ' ))
+street_name = input('введите название улицы: ')
+block_number = int(input('введите номер дома: '))
+print('ваш адрес: ', street_name, block_number)
+
+salary = int(input('какой размер вашей текущей зарплаты: '))
+new_salary = int(input('какой размер вашей желаемой зарплаты: '))
+diff = new_salary - salary
+print("вам надо зарабатывать на ", diff, "больше")
+
+
